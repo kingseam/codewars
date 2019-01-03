e0a02fc5e33c130ff42fabe40826391fe479d0ba
LAPTOP-CTLE48NG\huntak <oht5080@gmail.com>
2019-01-03 16:30:58 +0900

    huntak's ReverseWords solution

---
+++ src/main/java/huntak/ReverseWords.java
@@ -0,0 +1,16 @@
+package suhyeon;
+
+import comm.ReverseWordsImpl;
+
+public class ReverseWords implements ReverseWordsImpl {
+	public static String reverseWords(String str){
+		String[] splitStr = str.split(" ");
+    	String answer = splitStr[0];
+    	
+    	for(int i = 1; i < splitStr.length; i++){
+    		answer = splitStr[i] + " " + answer;
+    	}
+        
+        return answer;
+	 }
+}
